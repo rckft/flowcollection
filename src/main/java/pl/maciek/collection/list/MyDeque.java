084f1e724fd63d173fe77db8ccb65735699e0e75
M S <Maciek>
2025-01-28 14:14:32 +0100

    MyDeque implementation

---
+++ src/main/java/pl/maciek/collection/list/MyDeque.java
@@ -0,0 +1,29 @@
+package pl.maciek.collection.list;
+
+public interface MyDeque<T> extends MyQueue<T> {
+
+    void addFirst(T e);
+
+    void addLast(T e);
+
+    boolean offerFirst(T e);
+
+    boolean offerLast(T e);
+
+    T removeFirst();
+
+    T removeLast();
+
+    T pollFirst();
+
+    T pollLast();
+
+    T getFirst();
+
+    T getLast();
+
+    T peekFirst();
+
+    T peekLast();
+
+}
